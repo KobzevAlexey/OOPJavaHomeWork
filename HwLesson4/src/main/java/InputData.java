20b28bd9e53b89e79a0f72fb824634c8418bab60
Alexey <kabzone23@mail.ru>
2023-03-14 18:50:41 +0300

    Задание выполнено

---
+++ HwLesson4/src/main/java/InputData.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InputData {
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static void inputData(ArrayList<Schedule> priority) {
+
+        Schedule schedule = new Schedule();
+    }
+}
